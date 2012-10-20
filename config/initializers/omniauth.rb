cdfdf464941be518f42ac242b66fe3ec872963f1
jhubert <jhubert@gmail.com>
2012-10-20 09:28:20 -0700

    Organize the configuration variables better

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
+# Make sure that the facebook settings have been loaded already
+require_relative './configuration' unless defined?(FACEBOOK_APP_ID)
+
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET
 end
